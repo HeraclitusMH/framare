c96fe8467d3ff4de062bbe4ccac3a7f9e362dc1f
HeraclitusMH <lucapindemonte@gmail.com>
2022-07-26 15:17:02 +0200

    last event and amministrazione trasparente

--- src/associazioneTrasparente/AssociazioneTrasparente.js
+++ src/associazioneTrasparente/AssociazioneTrasparente.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import {Container,Row,Col} from "react-bootstrap";
 import "./associazioneTrasparente.css"
+import statuto from "../assets/pdf/StatutoAssociazioneFramarè.pdf"
+import r2019 from "../assets/pdf/Rendiconto2019.pdf"
+import r2020 from "../assets/pdf/Rendiconto2020.pdf"
+import r2021 from "../assets/pdf/Rendiconto2021.pdf"
+import statopat2021 from "../assets/pdf/Statopatrimoniale2021.pdf"
 
 export class AssociazioneTrasparente extends Component {
     render() {
@@ -11,7 +16,23 @@ export class AssociazioneTrasparente extends Component {
                     <Col xs={12} md={6}>
                         <h2 className={"title"}>Associazione trasparente</h2> <br/><br/>
                         <p>
-                            Associazione trasparente
+                            In questa pagina l’Associazione di Promozione Sociale Francesco Maresca - Framarè
+                            APS rende pubblico il materiale secondo le normative vigenti ai fini di trasparenza e
+                            diffusione dell’informazione.
+                            <br/><br/>
+                            L’Associazione Framarè APS, costituita in data 26 ottobre 2019, è stata iscritta al Registro
+                            Unico Nazionale per il Terzo Settore.
+                            <br/><br/>
+                            <i>Ai sensi e per gli effetti dell’articolo 47 del D. Lgs n. 117/2017 e dell’articolo 9 del D.M. n.
+                            106 del 15/09/2020 l’Ufficio Regionale del Runts Lazio ha provveduto con determina
+                            all’iscrizione dell’Ente ASSOCIAZIONE DI PROMOZIONE SOCIALE FRANCESCO
+                            MARESCA- FRAMARE&#39; APS (C.F. 96436370587; rep. n. 29550 prot. n.
+                            0390481\21/04/2022) con sede in VIA TREVISO 33 00161 ROMA (RM), nella sezione
+                            ASSOCIAZIONI DI PROMOZIONE SOCIALE del RUNTS.</i><br/><br/>
+                            <a href={statuto}>Statuto dell’associazione Framarè Aps iscritta al Runts</a>
+                            <br/><br/>
+                            Rendiconti: <br/>
+                            <a href={r2019}>Rendiconto 2019</a>, <a href={r2020}>Rendiconto 2020</a>, <a href={r2021}>Rendiconto 2021</a>, <a href={statopat2021}>Stato patrimoniale 2021</a>
                         </p>
                         <br/><br/>
                     </Col>
--- src/eventienotizie/EventiNotizie.js
+++ src/eventienotizie/EventiNotizie.js
@@ -6,29 +6,113 @@ import foto10 from '../assets/foto/fra10.jpg';
 import sl from '../assets/saintL.png';
 import targa from '../assets/foto/Targa.jpg';
 import separator from '../assets/paint-brush-stroke-4-23.png';
+import locandina1agostocelimontana from '../assets/foto/Concerto1agosto2022sAxFriends.png'
 
 export class EventiNotizie extends Component {
     render() {
         return(
             <div>
                 <Container className={"maxSize pageContainer"} id={"eventinotizie"}>
-                    <h2 className={"title"}>Eventi e notizie</h2> <br/><br/>
+                    <h2 className={"title"}>Eventi e notizie</h2>
+                    <br/><br/>
                     <Container className={"event"}>
                         <Row>
-                            <Col xs={12} md={3}></Col>
-                            <Col xs={12} md={6}>
+                            <Col xs={12} md={2}></Col>
+                            <Col xs={12} md={8}>
+                                <h4 className={"text-center"}>Note di mezza estate. Concerto per sax &amp; friends</h4>
+                                <img src={locandina1agostocelimontana} alt="" className={"foto"}/>
+                                <br/><br/>
                                 <p>
-Il 26 giugno 2019, gli amici di Matemù, il centro giovani e scuola d’arte del CIES ci hanno fatto una 
-bella sorpresa: hanno inaugurato nella loro sede una nuova stanza musicale dedicata a Francesco e Anna,
- affinchè la loro musica continui … <br/><br/>
-Ringraziamo di cuore gli amici di <a href="https://www.cies.it/matemu/">Matemù</a> per la grande sensibilità e l’affetto sincero che sanno regalare a tutti e vi invitiamo a seguire le loro preziose iniziative per tutti i ragazzi, 
-per costruire comunità e integrazione grazie all’arte e alla musica. 
-Francesco aveva condiviso con molti ragazzi e musicisti di Matemù (il carissimo Carlo Conti) il sogno di potersi esprimere con il 
-suo sax condividendo il magnifico spirito di gruppo che si respira in questo centro giovanile.
-<img src={targa} alt="" className={"foto"}/>
+                                    Spaziando fra le sonorità del jazz e della classica, del cantautorato e del rock progressivo,
+                                    giovani talenti della scena romana si alternano in formazioni diverse che accompagnano il
+                                    pubblico in un’originale esplorazione delle potenzialità tecniche ed espressive del
+                                    sassofono.<br/><br/>
+                                    L’evento, promosso dall’Associazione Framarè e dalla Fondazione Anna Donzelli, in
+                                    collaborazione con Village Celimontana, che si ringrazia per la calorosa disponibilità ad
+                                    accogliere lo spirito dell’iniziativa, è il primo passo del progetto Note di mezza estate, che
+                                    mette al centro la funzione sociale della musica, intesa come fecondo incontro di persone
+                                    e culture diverse, alla maniera di Anna Donzelli e Francesco Maresca, cui il concerto è
+                                    dedicato.
+                                </p>
+
+                                <p>
+                                    Evento ospitato da Village Celimontana e promosso da @AssociazioneFramare (pagina) e
+                                    Fondazione Anna Donzelli.
+                                    <br/><br/>
+                                    <h4><i>Con la partecipazione di:</i></h4><br/>
+                                    Voci:<br/>
+                                    <a href="https://www.facebook.com/johnnydalbasso1">Johnny DalBasso</a><br/>
+                                    Valentina Gargano<br/>
+                                    <br/>
+                                    Sax:<br/>
+                                    <a href="https://www.facebook.com/simone.bellagamba.14">Simone Bellagamba</a>
+                                    <br/>
+                                    <a href="https://www.facebook.com/el.berna.3">Ivan Bernardini</a><br/>
+                                    <a href="https://www.facebook.com/marco.bonelli.35">Marco Bonelli</a><br/>
+                                    <a href="https://www.facebook.com/cosimo.fiaschi.71">Cosimo Fiaschi</a><br/>
+                                    <br/>
+                                    Archi:<br/>
+                                    <a href="https://www.facebook.com/maddalena.fogacciceli">Maddalena Fogacci Celi</a><br/>
+                                    <a href="https://www.facebook.com/ludovica.luppi">Ludovica Luppi</a><br/>
+                                    <a href="https://www.facebook.com/profile.php?id=100006255105921">Chiara Mazzocchi</a><br/>
+                                    <a href="https://www.facebook.com/elisabetta.paolini.7">Elisabetta Paolini</a><br/>
+                                    <br/>
+                                    Chitarre:<br/>
+                                    <a href="https://www.facebook.com/emanuelecasaliartist">Emanuele Casali - artist</a><br/>
+                                    <a href="https://www.facebook.com/michele.proietti.315">Michele Proietti</a><br/>
+                                    <br/>
+                                    Tastiere:<br/>
+                                    <a href="https://www.facebook.com/emanuele.guarnieri">Emanuele Guarnieri</a><br/>
+                                    <br/>
+                                    Tromba:<br/>
+                                    <a href="https://www.facebook.com/frnlvc">Ludovico Franco</a><br/>
+                                    <br/>
+                                    Basso:<br/>
+                                    <a href="https://www.facebook.com/andreacasali25">Andrea Casali</a><br/>
+                                    <br/>
+                                    Modulari:<br/>
+                                    <a href="https://www.facebook.com/ruggerofornarimusic">Ruggero Fornari</a><br/>
+                                    <br/>
+                                    E con la <a href="https://www.facebook.com/profile.php?id=100078369005420">Metropolitan Jazz Orchestra</a><br/>
+                                    Diretta da Antonello Sorrentino<br/>
+                                    <br/>
+                                    Arrangiamenti di:<br/>
+                                    <a href="https://www.facebook.com/marco.bonelli.35">Marco Bonelli</a><br/>
+                                    <a href="https://www.facebook.com/james.scocciaferro.9">Giordano De Nisi</a><br/>
+                                    <a href="https://www.facebook.com/profile.php?id=100063697626381">Valentina Ramunno</a><br/>
+                                    <a href="https://www.facebook.com/spesale.music">Alessandro Speranza Music</a><br/>
+                                    <br/>
+                                    Programma, organizzazione e coordinamento: <a href="https://www.facebook.com/profile.php?id=1657748989">Emma Passi</a>,
+                                    <a href="https://www.facebook.com/michele.proietti.315"> Michele Proietti</a>, <a href="https://www.facebook.com/profile.php?id=1192266981">Paolo Zani</a>
+                                </p>
+                            </Col>
+                            <Col xs={12} md={2}></Col>
+                        </Row>
+                    </Container>
+
+                    <Row>
+                        <Col xs={12} md={4} className={"pl0 pr0"}></Col>
+                        <Col xs={12} md={4} className={"pl0 pr0"}>
+                            <img src={separator} alt="" className={"separator"}/>
+                        </Col>
+                    </Row>
+
+                    <Container className={"event"}>
+                        <Row>
+                            <Col xs={12} md={2}></Col>
+                            <Col xs={12} md={8}>
+                                <p>
+                                    Il 26 giugno 2019, gli amici di Matemù, il centro giovani e scuola d’arte del CIES ci hanno fatto una
+                                    bella sorpresa: hanno inaugurato nella loro sede una nuova stanza musicale dedicata a Francesco e Anna,
+                                     affinchè la loro musica continui … <br/><br/>
+                                    Ringraziamo di cuore gli amici di <a href="https://www.cies.it/matemu/">Matemù</a> per la grande sensibilità e l’affetto sincero che sanno regalare a tutti e vi invitiamo a seguire le loro preziose iniziative per tutti i ragazzi,
+                                    per costruire comunità e integrazione grazie all’arte e alla musica.
+                                    Francesco aveva condiviso con molti ragazzi e musicisti di Matemù (il carissimo Carlo Conti) il sogno di potersi esprimere con il
+                                    suo sax condividendo il magnifico spirito di gruppo che si respira in questo centro giovanile.
+                                    <img src={targa} alt="" className={"foto"}/>
                                 </p> 
                             </Col>
-                            <Col xs={12} md={3}></Col>
+                            <Col xs={12} md={2}></Col>
                         </Row>
                     </Container>
                     <Row>
@@ -39,8 +123,8 @@ suo sax condividendo il magnifico spirito di gruppo che si respira in questo cen
                     </Row>
                     <Container className={"event"}>
                         <Row>
-                            <Col xs={12} md={3}></Col>
-                            <Col xs={12} md={6}>
+                            <Col xs={12} md={2}></Col>
+                            <Col xs={12} md={8}>
                                <p>  
 Il 29 giugno 2019 la <a href="https://www.slmc.it">Scuola di musica Saint Louis College of Music</a> ha conferito la laurea postuma a Francesco,
  dedicando all’evento una specifica serata al Monk.<br/><br/>
@@ -69,8 +153,8 @@ Vogliamo ringraziare pubblicamente la scuola di musica Saint Louis College of Mu
                     </Row>
                     <Container className={"event"}>
                         <Row>
-                            <Col xs={12} md={3}></Col>
-                            <Col xs={12} md={6}>
+                            <Col xs={12} md={2}></Col>
+                            <Col xs={12} md={8}>
                                 <p>
                                     Nell’ambito della rassegna musicale Sorrento Jazz, il 13 dicembre 2019, è stata presentata la
                                     nuova Associazione di promozione sociale “Framarè”, dedicata a Francesco Maresca, la giovane
@@ -94,7 +178,7 @@ Vogliamo ringraziare pubblicamente la scuola di musica Saint Louis College of Mu
                                     Alessio Crespi (batteria)<br/><br/>
                                 </p> <br/><br/>
                             </Col>
-                            <Col xs={12} md={3}></Col>
+                            <Col xs={12} md={2}></Col>
                         </Row>
                         <Row>
                         <Col xs={12} md={2} className={"pl0 pr0"}></Col>
@@ -106,13 +190,13 @@ Vogliamo ringraziare pubblicamente la scuola di musica Saint Louis College of Mu
                 {/*
                 <Container className={"event"}>
                     <Row>
-                        <Col xs={12} md={3}></Col>
-                        <Col xs={12} md={6}>
+                        <Col xs={12} md={2}></Col>
+                        <Col xs={12} md={8}>
                             <p>
                                 
                             </p> 
                         </Col>
-                        <Col xs={12} md={3}></Col>
+                        <Col xs={12} md={2}></Col>
                     </Row>
                     <Row>
                     <Col xs={12} md={2} className={"pl0 pr0"}></Col>
